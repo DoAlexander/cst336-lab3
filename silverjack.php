0c7bac3a03c65fa3dd8cfa96ae19ea0370c958a1
alex7111 <doalexander@csumb.edu>
2016-09-20 18:22:35 +0000

    Trying to work on suits

--- silverjack.php
+++ silverjack.php
@@ -20,21 +20,29 @@ class card {
 }
 
 class deck {
+    private $suits = [];
     private $cards = [];
     
-    function __construct() {
+    public function __construct() {
         for($i = 0; $i < 4; $i++) {
             for($y = 0; $y < 13; $y++) {
                 $this->cards[] = new card($i, $y);
                 
             }
         }
+        array_push($this->suits, 'clubs', 'diamonds', 'hearts', 'spades');
     }
     
-    function getDeck() {
+    public function getDeck() {
+        echo('<table>');
         foreach($this->cards as $card) {
             echo($card->getSuit());
+            echo('<tr><img id="statImage" src="./img/cards/');
+            echo($suits[$card->getSuit()]);
+            echo('/'.($card->getCost()+1).'.png" id="" ></img>');
+            echo('</tr>');
         }
+        echo('</table>');
         
     }
 }
